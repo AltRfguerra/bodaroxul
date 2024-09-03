a7884a1c4542be8a728e93da4ebc4aaf27d1a8f1
AltRfguerra <alt.rfguerra@gmail.com>
2024-09-03 15:59:04 -0600

    hola

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,28 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import { SITE } from './src/config';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), sitemap()],
+  integrations: [
+    mdx(),
+    sitemap()
+  ],
   server: {
     host: '0.0.0.0',
   },
-  site: SITE.url,
+  site: 'https://altrfguerra.github.io/bodaroxul/',  // Asegúrate de que esta sea la URL correcta de tu sitio
+  output: 'static',
+  outDir: 'docs',  // Define 'docs' como la carpeta de salida
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'src/styles/sass/main.scss',  // Ruta al archivo principal SCSS
+      },
+      output: {
+        dir: 'docs/_astro',  // Carpeta donde se generarán los archivos JS y CSS
+        format: 'esm',  // Formato de salida del script JS
+      }
+    }
+  }
 });
